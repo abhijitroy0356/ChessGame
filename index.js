f53746a2fdb0cd3fd00ad5fe213cdd40d963ce7e
Abhijit Roy <60839264+abhijitroy0356@users.noreply.github.com>
2023-05-29 20:20:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const ghora =document.querySelectorAll('.ghora')
+const hathi =document.querySelectorAll('.hathi')
+const king = document.querySelectorAll('.king')
+const mantri=document.querySelectorAll('.mantri')
+const nouka=document.querySelectorAll('.nouka')
+const pawn=document.querySelectorAll('.pawn')
